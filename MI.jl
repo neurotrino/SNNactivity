dbf35159986da62142f637522af4b3e0e0c19413
Kyle Bojanek <kyle@Kyles-MacBook-Pro.local>
2018-08-19 16:13:09 -0500

    wrappers for MI stuff as well as bug fix for branching (can now compute without assuming you are using pre assigned graph)

--- MI.jl
+++ MI.jl
@@ -1,22 +1,48 @@
-#what to do about 0. alpha + (1-alpha)*p or should i filter NaN
-#check post pre conventions
+using ProgressMeter
 
-function signed(graph,raster)
-	# takes MI graph returns signed MI graph
-	N = size(graph)[1]
+function binary_raster_gen(spike_set,discretize_dt)
+	bins = 0:discretize_dt:run_total
+	raster = zeros(length(spike_set),length(bins)-1)
+	for i = 1:length(spike_set)
+		discrete_train = unique(round.(spike_set[i]/discretize_dt))
+		discrete_train = discrete_train[discrete_train.>0]
+
+		for j = 1:length(discrete_train)
+			raster[i,Int(discrete_train[j])] = 1
+		end
+	end
+	return raster
+end
+
+function main(spikes,dt)
+	raster = binary_raster_gen(spikes,dt)
+	MI_graph = confMI_mat(raster)
+	signed_graph = signed_MI(MI_graph)
+	pos_graph = pos(signed_graph)
+	reexpress_graph = reexpress_param(pos_graph)
+	background_graph = background(reexpress_graph)
+	residual_graph = residual(background_graph,MI_graph)
+	normed_MI_graph = normed_residual(residual_graph)
+	return normed_MI_graph
+end
+
+function signed_MI(graph,raster)
+	# takes MI graph returns signed_MI MI graph
+	neurons = size(graph)[1]
 	signed_graph = copy(graph)
-	for i = 1:N
-		for j = i:N
-			corr_mat = cor(hcat(raster[i,:],raster[j,:]))
-			signed_graph[j,i] *= sign(corr_mat[1,2])
-			signed_graph[i,j] *= sign(corr_mat[2,1])
+	@showprogress 1 "Computing signed_MI MI Graph" for post = 1:neurons
+		for pre = (post+1):neurons
+			corr_mat = cor(hcat(raster[pre,:],raster[post,:]))
+			factor = sign(corr_mat[1,2])
+			signed_graph[post,pre] *= factor
+			signed_graph[pre,post] *= factor
 		end
 	end
 	return signed_graph
 end
 
 function pos(graph)
-	#takes signed MI graph, returns positive version
+	#takes signed_MI MI graph, returns positive version
 	pos_graph = copy(graph)
 	pos_graph[find(x->x<0,graph)] = 0
 	return pos_graph 
@@ -31,7 +57,7 @@ function reexpress_param(graph)
 	lower_exp=0.00000000000001
 	upper_skew = skewness(data.^upper_exp)
 	lower_skew = skewness(data.^lower_exp)
-	for i = 1:steps
+	@showprogress 1 "Computing Reexpress Parameter" for i = 1:steps
 		new_exp = (upper_exp + lower_exp)/2
 		new_skew = skewness(data.^new_exp)
 	    if new_skew<0
@@ -49,17 +75,19 @@ function reexpress_param(graph)
 	end
 	print(reexpress,"\n\n")
 	reexpress_graph = copy(graph)
-	reexpress_graph = reexpress_graph.^reexpress_graph
+	reexpress_graph = reexpress_graph.^reexpress
 	return reexpress_graph
 end
 
 function background(graph)
 	#takes reexpress graph returns background graph
 	background = copy(graph)
-	N = size(graph)[1]
-	for i = 1:N
-		for j = 1:N
-			background[j,i] = mean(graph[j,1:N .!= i])*mean(graph[1:N .!= j,i])
+	neurons = size(graph)[1]
+	@showprogress 1 "Computing Background Graph" for pre = 1:neurons
+		for post = 1:neurons
+			if (post != pre)
+				background[post,pre] = mean(graph[post,1:N .!= pre])*mean(graph[1:N .!= post,pre])
+			end
 		end
 	end
 	return background
@@ -69,9 +97,9 @@ function residual(background_graph,graph)
 	#takes background graph and MI graph
 	#returns residual graph
 	residual = copy(graph)
-	N = size(graph)[1]
+	neurons = size(graph)[1]
 	data_y = graph[:]
-	data_x = hcat(background_graph[:],ones(N*N))
+	data_x = hcat(background_graph[:],ones(neurons*neurons))
 	coefs = data_x\data_y
 	m = coefs[1]
 	b = coefs[2]
@@ -83,10 +111,12 @@ function normed_residual(graph)
 	#tales residual graph returns
 	#normalized residual graph
 	norm_residual = copy(graph)
-	N = size(graph)[1]
-	for i = 1:N
-		for j = 1:N
-			norm_residual[j,i] = std(graph[j,1:N .!= i])*std(graph[1:N .!= j,i])
+	neurons = size(graph)[1]
+	@showprogress 1 "Normalizing Residual Graph" for pre = 1:neurons
+		for post = 1:neurons
+			if (post != pre)
+				norm_residual[post,pre] = std(graph[post,1:N .!= pre])*std(graph[1:N .!= post,pre])
+			end
 		end
 	end
 	cutoff = median(norm_residual)
@@ -99,15 +129,20 @@ function MI(train_1,train_2,lag,alpha)
 	states = [0,1]
 	for i = 1:length(states)
 		i_inds = find(x->x==states[i],train_1)
-		p_i = length(i_inds)/length(train_1)
-		for j = 1:length(states)
-			j_inds = find(x->x==states[j],train_2[1+lag:end]) - lag
-			p_j = length(j_inds)/(length(train_2)-lag)
-			p_i_and_j = length(intersect(i_inds,j_inds))/(length(train_1)-lag)
-			if (alpha > 0)
-				MI = MI + alpha + (1-alpha) * p_i_and_j * log2(p_i_and_j/(p_i*p_j))
-			elseif (p_i_and_j > 0)
-				MI += p_i_and_j * log2(p_i_and_j/(p_i*p_j))
+		if (length(i_inds) > 0)
+			p_i = length(i_inds)/length(train_1)
+			for j = 1:length(states)
+				j_inds = find(x->x==states[j],train_2) - lag
+				if length(j_inds) > 0
+					j_inds = j_inds[j_inds>0]
+					p_j = length(j_inds)/(length(train_2)-lag)
+					p_i_and_j = length(intersect(i_inds,j_inds))/(length(train_1)-lag)
+					if (alpha > 0)
+						MI = MI + alpha + (1-alpha) * p_i_and_j * log2(p_i_and_j/(p_i*p_j))
+					elseif (p_i_and_j > 0)
+						MI += p_i_and_j * log2(p_i_and_j/(p_i*p_j))
+					end
+				end
 			end
 		end
 	end 
@@ -120,19 +155,21 @@ function confMI(train_1,train_2,lag,alpha)
 	for i = 1:length(states)
 		i_inds = find(x->x==states[i],train_1)
 		p_i = length(i_inds)/length(train_1)
-		for j = 1:length(states)
-			j_inds = find(x->x==states[j],train_2)
-			j_inds_lagged = j_inds - lag
-			if j_inds_lagged[1] == 0
-				j_inds_lagged = j_inds_lagged[2:end]
-			end
-			j_inds = union(j_inds,j_inds_lagged)
-			p_j = length(j_inds)/(length(train_2)-lag)
-			p_i_and_j = length(intersect(i_inds,j_inds))/(length(train_1)-lag)
-			if (alpha > 0)
-				MI = MI + alpha + (1-alpha) * p_i_and_j * log2(p_i_and_j/(p_i*p_j))
-			elseif (p_i_and_j > 0)
-				MI += p_i_and_j * log2(p_i_and_j/(p_i*p_j))
+		if (length(i_inds) > 0)
+			for j = 1:length(states)
+				j_inds = find(x->x==states[j],train_2)
+				j_inds_lagged = j_inds - lag
+				if (length(j_inds) > 0)
+					j_inds_lagged = j_inds_lagged[j_inds_lagged.>0]
+					j_inds = union(j_inds,j_inds_lagged)
+					p_j = length(j_inds)/(length(train_2)-lag)
+					p_i_and_j = length(intersect(i_inds,j_inds))/(length(train_1)-lag)
+					if (alpha > 0)
+						MI = MI + alpha + (1-alpha) * p_i_and_j * log2(p_i_and_j/(p_i*p_j))
+					elseif (p_i_and_j > 0)
+						MI += p_i_and_j * log2(p_i_and_j/(p_i*p_j))
+					end
+				end
 			end
 		end
 	end 
@@ -142,41 +179,44 @@ end
 function confMI_mat(raster)
 	lag = 1
 	alpha = 0
-	N = size(raster)[1]
-	mat = zeros(N,N)
-	for post = 1:N
-		for pre = 1:N
+	neurons = size(raster)[1]
+	mat = zeros(neurons,neurons)
+	@showprogress 1 "Computing Confluent MI"  for post = 1:neurons
+		for pre = 1:neurons
 			if post != pre
-				N[post,pre] = confMI(raster[pre,:],raster[post,:],lag,alpha)
+				mat[post,pre] = confMI(raster[pre,:],raster[post,:],lag,alpha)
 			end
 		end
 	end
+	return mat
 end
 
 function consecMI_mat(raster)
 	lag = 1
 	alpha = 0
-	N = size(raster)[1]
-	mat = zeros(N,N)
-	for post = 1:N
-		for pre = 1:N
+	neurons = size(raster)[1]
+	mat = zeros(neurons,neurons)
+	@showprogress 1 "Computing Consectuve MI" for post = 1:neurons
+		for pre = 1:neurons
 			if post != pre
-				N[post,pre] = MI(raster[pre,:],raster[post,:],lag,alpha)
+				mat[post,pre] = MI(raster[pre,:],raster[post,:],lag,alpha)
 			end
 		end
 	end
+	return mat
 end
 
 function simulMI_mat(raster)
 	lag = 0
 	alpha = 0
-	N = size(raster)[1]
-	mat = zeros(N,N)
-	for post = 1:N
-		for pre = 1:N
+	neurons = size(raster)[1]
+	mat = zeros(neurons,neurons)
+	@showprogress 1 "Computing Simultaneous MI" for post = 1:neurons
+		for pre = 1:neurons
 			if post != pre
-				N[post,pre] = MI(raster[pre,:],raster[post,:],lag,alpha)
+				mat[post,pre] = MI(raster[pre,:],raster[post,:],lag,alpha)
 			end
 		end
 	end
+	return mat
 end
